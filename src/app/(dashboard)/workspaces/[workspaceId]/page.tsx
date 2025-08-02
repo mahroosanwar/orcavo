1fb95f8071fc03a1312b202f58419b3665b55bdb
Mahroos Anwar <mahroos.anwar0901@gmail.com>
2025-08-03 02:32:50 +0530

    feat: implement workspace creation and standalone layout pages with user authentication

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,5 +1,20 @@
-const WorkspaceIdPage = () => {
-  return <div>WorkspaceIdPage</div>;
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/queries";
+
+// import { WorkspaceIdClient } from "./client";
+
+const WorkspaceIdPage = async () => {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
+  return (
+    <div>
+      WorkspaceIdPage
+      {/* <WorkspaceIdClient />; */}
+    </div>
+  );
 };
 
 export default WorkspaceIdPage;
