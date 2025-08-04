25132d58f128040f83fe7431909991749c381b04
Mahroos Anwar <mahroos.anwar0901@gmail.com>
2025-08-04 11:46:58 +0530

    feat: implement members management functionality with member listing, updating, and deletion

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import auth from "@/features/auth/server/route";
-// import members from "@/features/members/server/route";
+import members from "@/features/members/server/route";
 import workspaces from "@/features/workspaces/server/route";
 // import projects from "@/features/projects/server/route";
 // import tasks from "@/features/tasks/server/route";
@@ -13,7 +13,7 @@ const app = new Hono().basePath("/api");
 const routes = app
   .route("/auth", auth)
   .route("/workspaces", workspaces)
-//   .route("/members", members)
+  .route("/members", members)
 //   .route("/projects", projects)
 //   .route("/tasks", tasks);
 
